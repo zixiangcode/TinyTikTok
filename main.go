4744254d8c1d337aaf5610785baa019a2e7e6c97
zixiangcode <zixiangcode@gmail.com>
2023-08-01 17:27:35 +0800

    更新 router 路径

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/RaymondCode/simple-demo/router"
 	"github.com/RaymondCode/simple-demo/service"
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +11,7 @@ func main() {
 
 	r := gin.Default()
 
-	initRouter(r)
+	router.InitRouter(r)
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
