ff7c801469b18ed85a8fef1900d6c8edb2cae077
zixiangcode <zixiangcode@gmail.com>
2023-08-08 17:32:21 +0800

    新增 User 类

---
+++ models/User.go
@@ -0,0 +1,17 @@
+package models
+
+type User struct {
+	// ID              int64  `json:"id"`               // 用户id
+	CommonEntity           // 使用 CommonEntity 对象代替 ID
+	Password        string `json:"password"`         // 用户密码
+	Avatar          string `json:"avatar"`           // 用户头像
+	BackgroundImage string `json:"background_image"` // 用户个人页顶部大图
+	FavoriteCount   int64  `json:"favorite_count"`   // 喜欢数
+	FollowCount     int64  `json:"follow_count"`     // 关注总数
+	FollowerCount   int64  `json:"follower_count"`   // 粉丝总数
+	IsFollow        bool   `json:"is_follow"`        // true-已关注，false-未关注
+	Name            string `json:"name"`             // 用户名称
+	Signature       string `json:"signature"`        // 个人简介
+	TotalFavorited  string `json:"total_favorited"`  // 获赞数量
+	WorkCount       int64  `json:"work_count"`       // 作品数
+}
