eb82bc643f52049802a5a33b5a23571a9c5b8059
zixiangcode <zixiangcode@gmail.com>
2023-08-09 23:13:58 +0800

    修复解耦 Response 后带来的影响

--- controller/comment.go
+++ controller/comment.go
@@ -1,17 +1,18 @@
 package controller
 
 import (
+	"TinyTikTok/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type CommentListResponse struct {
-	Response
+	models.Response
 	CommentList []Comment `json:"comment_list,omitempty"`
 }
 
 type CommentActionResponse struct {
-	Response
+	models.Response
 	Comment Comment `json:"comment,omitempty"`
 }
 
@@ -23,7 +24,7 @@ func CommentAction(c *gin.Context) {
 	if user, exist := usersLoginInfo[token]; exist {
 		if actionType == "1" {
 			text := c.Query("comment_text")
-			c.JSON(http.StatusOK, CommentActionResponse{Response: Response{StatusCode: 0},
+			c.JSON(http.StatusOK, CommentActionResponse{Response: models.Response{StatusCode: 0},
 				Comment: Comment{
 					Id:         1,
 					User:       user,
@@ -32,16 +33,16 @@ func CommentAction(c *gin.Context) {
 				}})
 			return
 		}
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
+		c.JSON(http.StatusOK, models.Response{StatusCode: 0})
 	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+		c.JSON(http.StatusOK, models.Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
 	}
 }
 
 // CommentList all videos have same demo comment list
 func CommentList(c *gin.Context) {
 	c.JSON(http.StatusOK, CommentListResponse{
-		Response:    Response{StatusCode: 0},
+		Response:    models.Response{StatusCode: 0},
 		CommentList: DemoComments,
 	})
 }
