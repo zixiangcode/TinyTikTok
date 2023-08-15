ac6a7c3858578c48fc89cc0b463c290f9cd02f78
zzzly <774408640@qq.com>
2023-08-15 09:53:28 +0800

    增加Comment表的必要信息

--- models/Comment.go
+++ models/Comment.go
@@ -5,5 +5,18 @@ type Comment struct {
 	//ID         int64  `json:"id"`          // 评论id
 	Content string `json:"content"` // 评论内容
 	//CreateDate string `json:"create_date"` // 评论发布日期，格式 mm-dd
-	User User `json:"user"` // 评论用户信息
+	//User User `json:"user"` // 评论用户信息
+	UserID  int64 `json:"user_id"`
+	VideoID int64 `json:"video_id"`
+}
+
+func (table *Comment) TableName() string {
+	return "comments"
+}
+
+type MyComment struct {
+	Id         uint64 `json:"id,omitempty"`
+	User       User   `json:"user"`
+	Content    string `json:"content,omitempty"`
+	CreateDate string `json:"create_date,omitempty"`
 }
