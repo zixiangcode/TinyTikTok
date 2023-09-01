186ffe96f44e8a6e8975e5b06a158beae381c784
zixiangcode <zixiangcode@gmail.com>
2023-09-01 15:56:55 +0800

    修复 feed 的越界访问 bug

--- dao/FeedDAO.go
+++ dao/FeedDAO.go
@@ -10,7 +10,7 @@ import (
 func GetFeedVideosInfo(latestTime time.Time) ([]models.Video, error) {
 	var videos []models.Video
 	result := db.GetMysqlDB().
-		Where("create_time < ?", latestTime).
+		Where("create_time < ? AND is_deleted <> 1", latestTime).
 		Order("create_time desc").
 		Limit(config.VideoCount).
 		Find(&videos)
